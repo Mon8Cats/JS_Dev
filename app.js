62ae3eba912d77761272fc2dd11fdc4c05c756ad
Stone Kim <Mon8Cats@gmail.com>
2019-09-01 16:32:39 -0500

    add js_summary

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+//var myVar = document.getElementById("myVar");
+//console.log(myVar);
+//myVar.innerHTML = "Yes You can";
+
+function myFunc() {
+  return 1 + 1;
+}
+
+console.log(myFunc());
